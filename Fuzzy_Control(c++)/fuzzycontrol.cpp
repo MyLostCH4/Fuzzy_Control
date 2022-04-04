b8f5ce20a8984e1ab6a94fc7709b7c07163d28d7
mama <1578809593@qq.com>
2022-04-04 21:09:34 +0800

    模糊计算库，很不完善

---
+++ Fuzzy_Control(c++)/fuzzycontrol.cpp
@@ -0,0 +1,18 @@
+#include"fuzzycontrol.h"
+
+fuzzy::fuzzy(){}
+ 
+
+fuzzy::~fuzzy() {}
+//模糊矩阵的计算
+void fuzzy::fm_calculate()
+{
+
+}
+ 
+ 
+//模糊规则
+void fuzzy::Set_Rules()
+{
+
+}
---
+++ Fuzzy_Control(c++)/main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+#include"fuzzycontrol.h"
+void main()
+{
+
+}
